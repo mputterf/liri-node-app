d0c7cc121fcd0feaceaed51754947c6470ad9c6d
mputterf <michael.putterflam@gmail.com>
2019-04-08 22:01:27 -0700

    basic switch made

--- liri.js
+++ liri.js
@@ -3,4 +3,35 @@ require("dotenv").config();
 
 // import keys for spotify
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+
+// Get the type of search the user wants
+var search = process.argv[2];
+// Get the thing they user is searching for
+var term = process.argv.slice(3).join(" ");
+
+switch (search){
+    case "concert-this":
+        console.log("Searching the Bands in Town Artist Events API");
+
+        break;
+
+    case "spotify-this-song":
+        console.log("Searching Spotify");
+        // Create spotify obj
+        var spotify = new Spotify(keys.spotify);
+
+        break;
+
+    case "movie-this":
+        console.log("Searching OMDB for movie");
+
+        break;
+
+    case "do-what-it-says":
+        console.log("Executing command in random.txt");
+
+        break;
+
+    default:
+        console.log("Usage: node liri <concert-this/spotify-this-song/movie-this/do-what-it-says> <search terms>");
+}
