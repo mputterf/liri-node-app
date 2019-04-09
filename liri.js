7c70a48ac91c964c9e91cb21a4952abce9b609ff
mputterf <michael.putterflam@gmail.com>
2019-04-09 00:47:36 -0700

    concert-this working

--- liri.js
+++ liri.js
@@ -1,8 +1,13 @@
 // read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-// import keys for spotify
-var keys = require("./keys.js");
+// axios
+var axios = require("axios");
+// moment
+var moment = require("moment");
+
+// import key for spotify
+var keys = require("./key.js");
 
 // Get the type of search the user wants
 var search = process.argv[2];
@@ -12,6 +17,25 @@ var term = process.argv.slice(3).join(" ");
 switch (search){
     case "concert-this":
         console.log("Searching the Bands in Town Artist Events API");
+        var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp"
+
+        axios.get(URL).then(function(response){
+            // response.data is an array!
+            // console.log(response.data);
+            var jsonData = response.data;
+
+            for (var i = 0; i < jsonData.length; i++){
+                // Print venue name
+                console.log("Venue", jsonData[i].venue.name);
+                // Print venue location
+                console.log("Venue location: %s, %s", jsonData[i].venue.city, jsonData[i].venue.country);
+                // Use moment to formate the date as MM/DD/YYY and print it)
+                console.log("Date:", moment(jsonData[i].datetime).format('L'));
+                // A divider
+                console.log("------------------------------------");
+            }
+
+        });
 
         break;
 
